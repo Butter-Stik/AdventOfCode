6b70d27ba974808fcd59620c9a8f06bfcfa77395
Butter-Stik <baurluke@gmail.com>
2024-12-05 22:25:14 -0500

    day 4 and 5

--- day4/day4.py
+++ day4/day4.py
@@ -1,2 +1,15 @@
 from aocd import get_data
 inp = get_data(day=4, year=2015)
+import hashlib
+found1 = False
+i = 0
+while True:
+    val = int(hashlib.md5(str.encode(inp + str(i))).hexdigest(), 16)
+    if val <= int("0xfffffffffffffffffffffffffff", 16):
+        if not found1:
+            print(i)
+            found1 = True
+        if val <= int("0xffffffffffffffffffffffffff", 16):
+            print(i)
+            break
+    i += 1
--- day5/day5.py
+++ day5/day5.py
@@ -1,2 +1,29 @@
 from aocd import get_data
-inp = get_data(day=5, year=2015)
+import re
+inp = get_data(day=5, year=2015).splitlines()
+p1 = 0
+p2 = 0
+for line in inp:
+    if "ab" in line or "cd" in line or "pq" in line or "xy" in line:
+        continue
+    if len(re.findall("[aeiou]", line)) < 3:
+        continue
+    double = False
+    for i in range(len(line) - 1):
+        if line[i] == line[i + 1]:
+            double = True
+    if double == False:
+        continue
+    p1 += 1
+for line in inp:
+    double = False
+    for i in range(len(line) - 1):
+        if line.count(str(line[i]) + str(line[i + 1])) > 1:
+            double = True
+    gap = False
+    for i in range(len(line) - 2):
+        if line[i] == line[i + 2]:
+            gap = True
+    if gap and double:
+        p2 += 1
+print(p1, p2)
