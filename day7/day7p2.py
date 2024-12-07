2591a380fb161e8a4d904e5f37007570b8a820ca
Butter-Stik <baurluke@gmail.com>
2024-12-07 01:18:54 -0500

    day 7

--- day7/day7p2.py
+++ day7/day7p2.py
@@ -5,14 +5,34 @@ ops = []
 for line in inp:
     ops.append(re.findall("\d+", line))
 
+def countDigit(n):
+    if (n / 10 == 0):
+        return 1
+    return 1 + countDigit(n / 10)
+
+def int_to_ternary(n):
+  """Converts an integer to its ternary representation."""
+
+  if n == 0:
+    return "0"
+
+  ternary = ""
+  while n > 0:
+    n, remainder = divmod(n, 3)
+    ternary = str(remainder) + ternary
+
+  return ternary
+
 def operation(op, number):
-    binary = bin(number)[2:][::-1]
+    ternary = int_to_ternary(number)[::-1]
     signs = []
     for i in range(len(op)):
         signs.append("+")
-    for i in range(len(binary)):
-        if binary[i] == "1":
+    for i in range(len(ternary)):
+        if ternary[i] == "1":
             signs[i] = "*"
+        if ternary[i] == "2":
+            signs[i] = "||"
     return signs
 
 p1 = 0
@@ -21,14 +41,16 @@ for op in ops:
     target = int(op[0])
     op.pop(0)
     signs = []
-    for i in range(int(pow(2, len(op) - 1))):
+    for i in range(int(pow(3, len(op) - 1))):
         _op = operation(op, i)
         num = int(op[0])
         for n in range(len(op) - 1):
             if _op[n] == "+":
                 num += int(op[n + 1])
-            else:
+            elif _op[n] == "*":
                 num *= int(op[n + 1])
+            else:
+                num = int(str(num) + str(op[n + 1]))
         if num == target and not found:
             found = True
             p1 += target
