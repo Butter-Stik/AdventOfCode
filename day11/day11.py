4e2843f3e71055c81726a6dd5f3c7b24dd45b0d3
Butter-Stik <baurluke@gmail.com>
2024-12-11 09:52:21 -0500

    day 11

--- day11/day11.py
+++ day11/day11.py
@@ -1,2 +1,33 @@
 from aocd import get_data
-inp = get_data(day= ,year=2024)
+from functools import cache
+inp = get_data(day=11,year=2024).split()
+stones = {key: 1 for key in inp}
+newstones = {}
+blinks = 75
+@cache
+def get_newstone(stone):
+    newstone = []
+    if stone == '0':
+        newstone = ['1']
+    elif len(stone) % 2 == 0:
+        midpoint = len(stone) // 2
+        newstone = [str(int(stone[:midpoint])), str(int(stone[midpoint:]))]
+    else:
+        newstone = [str(int(stone) * 2024)]
+    return newstone
+    
+while blinks > 0:
+    for stone in stones:
+        num = stones[stone]
+        newstone = get_newstone(stone)
+        for new in newstone:
+            if new not in newstones:
+                newstones[new] = 0
+            newstones[new] = newstones[new] + num
+    stones = newstones.copy()
+    newstones = {}
+    blinks -= 1
+sum = 0
+for stone in stones:
+    sum += stones[stone]
+print(sum)
