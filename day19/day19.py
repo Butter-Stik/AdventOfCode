4309155febbfd0d81def6191a85fb3343bbfb6a8
Butter-Stik <baurluke@gmail.com>
2024-12-19 12:04:17 -0500

    day 19

--- day19/day19.py
+++ day19/day19.py
@@ -1,2 +1,24 @@
 from aocd import get_data
-inp = get_data(day= ,year=2024)
+from functools import cache
+inp = get_data(day=19,year=2024).split("\n\n")
+towels = inp[0].split(", ")
+patterns = inp[1].splitlines()
+@cache
+def can_make_pattern(pattern):
+    num = 0
+    if not pattern:
+        return 1
+    for i in range(len(pattern)):
+        substring = pattern[:i+1]
+        nextnum = can_make_pattern(pattern[i+1:])
+        if substring in towels and nextnum > 0:
+            num += nextnum
+    return num
+p1 = 0
+p2 = 0
+for pattern in patterns:
+    num = can_make_pattern(pattern)
+    p2 += num
+    if num > 0:
+        p1 += 1
+print(p1, p2)
