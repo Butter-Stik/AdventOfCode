a3d5a281b0d0f3e5a0fc591fa768a3d658bf175e
Luke Baur <baurluke@gmail.com>
2024-12-25 00:36:26 -0500

    :)

--- day25/day25.py
+++ day25/day25.py
@@ -1,2 +1,32 @@
 from aocd import get_data
-inp = get_data(day= ,year=2024)
+inp = get_data(day=25,year=2024).split('\n\n')
+keys = [schematic.splitlines() for schematic in inp if schematic[0] == "."]
+locks = [schematic.splitlines() for schematic in inp if schematic[0] == "#"]
+keyheights = []
+lockheights = []
+for key in keys:
+    heights = []
+    for c in range(len(key[0])):
+        r = len(key)
+        while key[r - 1][c] == "#":
+            r -= 1
+        heights.append(len(key) - 1 - r)
+    keyheights.append(heights)
+for lock in locks:
+    heights = []
+    for c in range(len(lock[0])):
+        r = 0
+        while lock[r + 1][c] == "#":
+            r += 1
+        heights.append(r)
+    lockheights.append(heights)
+p1 = 0
+for lock in lockheights:
+    for key in keyheights:
+        passed = True
+        for r in range(len(lock)):
+            if lock[r] + key[r] > 5:
+                passed = False
+        if passed:
+            p1 += 1
+print(p1)
